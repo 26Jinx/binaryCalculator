98074972fcbf4bf59ba19003debe7667ff9ce92d
PharahMain <26randomforests@gmail.com>
2020-11-06 18:17:59 -0800

    adding clear function to clear button

--- js/main.js
+++ js/main.js
@@ -9,7 +9,17 @@ function displayObj(str) {
 // create object to display
 function makeDisplayObj(obj) {
 
-    if (strToDisplay) {
+    if (obj === 'clr') {
+        strToDisplay = '';
+        calculationObj = {'numbers': [],
+            'operation': null,
+            'result': null};
+
+        console.log(calculationObj);
+
+        return displayObj(strToDisplay);
+
+    } else if (strToDisplay) {
         strToDisplay += obj.toString();
     } else {
         strToDisplay = obj.toString();
@@ -31,24 +41,43 @@ let calculationObj = {'numbers': [],
     'result': null};
 
 const btn0 = document.getElementById('btn0');
-btn0.addEventListener('click', function() {
+btn0.addEventListener('click', () => {
     makeDisplayObj(btn0.innerHTML);
-})
+}, false)
 
 const btn1 = document.getElementById('btn1');
-btn1.addEventListener('click', function() {
+btn1.addEventListener('click', () => {
     makeDisplayObj(btn1.innerHTML);
-});
+}, false);
 
 const btnSum = document.getElementById('btnSum');
-btnSum.addEventListener('click', function() {
+btnSum.addEventListener('click', () => {
     convertAndStore('add');
     makeDisplayObj(btnSum.innerHTML);
-});
+}, false);
 
 const btnSub = document.getElementById('btnSub');
-btnSub.addEventListener('click', function() {
-    convertAndStore('subtract')
-    makeDisplayObj(btnSub.innerHTML)
+btnSub.addEventListener('click', () => {
+    convertAndStore('subtract');
+    makeDisplayObj(btnSub.innerHTML);
+}, false)
+
+const btnMul = document.getElementById('btnMul');
+btnMul.addEventListener('click', () => {
+    convertAndStore('multiply');
+    makeDisplayObj(btnMul.innerHTML);
+}, false)
+
+const btnDiv = document.getElementById('btnDiv');
+btnDiv.addEventListener('click', () => {
+    convertAndStore('divide');
+    makeDisplayObj(btnDiv.innerHTML);
+}, false);
+
+const btnClr = document.getElementById('btnClr');
+btnClr.addEventListener('click', () => {
+    makeDisplayObj('clr');
 })
+
+
 
